33b38fdec08451a9446555ebcaeee29ff2c1a488
=muhammadumar-codes <=muhammadumar.codes@gmail.com>
2025-12-12 18:40:27 +0800

    code

--- Algorithms/InsertAlgorithem/InsertAlgorithem.js
+++ Algorithms/InsertAlgorithem/InsertAlgorithem.js
@@ -1,6 +1,22 @@
 // This code is the insert algrithem which is ujsed to insert  elements
 
-function insertSort() {}
+function insertSort() {
+  const array = [5, 3, 4, 1, 2]
+
+  for (let i = 0; i < array.length; i++) {
+    let key = array[i]
+    let j = i - 1
+
+    while (j >= 0 && array[j] > key) {
+      array[j + 1] = array[j]
+      j--
+    }
+
+    array[j + 1] = key
+  }
+
+  return array
+}
 
 // callig of it
-insertSort()
+console.log(insertSort())
