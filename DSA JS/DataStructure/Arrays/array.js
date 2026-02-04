f4df9c9262922141fcf95777448500f6d6ab1c38
muhammadumar-codes <muhammadumar.codes@gmail.com>
2026-02-04 09:54:05 +0500

    update

--- DSA JS/DataStructure/Arrays/array.js
+++
@@ -1,8 +0,0 @@
-const fruits = ['apple', 'banana', 'apple', 'orange', 'banana']
-const count = fruits.reduce((acc, curr) => {
-  acc[curr] = (acc[curr] || 0) + 1
-  return acc
-}, {})
-
-// This will output the correct count object: { apple: 2, banana: 2, orange: 1 }
-console.log(count) 
