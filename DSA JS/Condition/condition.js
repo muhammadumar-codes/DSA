2c9231b5a85309b4b5c38626e5c16fda11b09052
muhammadumar-codes <muhammadumar.codes@gmail.com>
2026-02-04 17:39:21 +0500

    update the code

---
+++ DSA JS/Condition/condition.js
@@ -0,0 +1,15 @@
+const age = 45
+switch (age) {
+  case { first: 40, secound: 45 }:
+    console.log('Your Age is 10')
+    break
+  case 20:
+    console.log('Your age is 20')
+    break
+  case 30:
+    console.log('Your age is 30')
+    break
+
+  default:
+    console.log('Invlide')
+}
